e739e56a93cb6668885708d14ad9d6169593ccbc
Amos Tang <amostang1994@gmail.com>
2020-05-01 12:06:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# build-a-basic-php-website
+ My first php website built from treehouse
---
+++ config/email_info.php
@@ -0,0 +1,7 @@
+<?php
+
+$from_email = "test@gmail.com"; // Replace with real one
+$from_password = "test"; // Replace with real one
+
+$to_email = "test@gmail.com"; // Replace with real one
+$to_name = "Test"; // Replace with real one
